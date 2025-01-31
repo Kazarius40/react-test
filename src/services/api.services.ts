15e25a17c4616d7a6e6305275b1c293669cc37ae
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-31 12:41:00 +0100

    працюю над логінацією

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -2,14 +2,18 @@ import axios from "axios";
 import {IUser} from "../models/user/IUser.ts";
 import {IAuthForm} from "../models/authorization/IAuthForm.ts";
 
+type LoginData = {
+    username: string;
+    password: string;
+    expiresInMins: number
+}
+
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    }
+    headers: {}
 })
 
-export const authService  = async (data: IAuthForm): Promise<IUser> => {
+export const authService = async (data: IAuthForm): Promise<IUser> => {
     try {
         const {data: user} = await axiosInstance.post<IUser>('/auth/login', data);
         return user;
